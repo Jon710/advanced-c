230a938939f1552cc427b3ecdfc903bffc40a1e1
Joao Luis Moraes <joaoluismoraes@Joaos-MacBook-Air.local>
2019-09-22 23:08:17 -0300

    pointers example 2

--- pointers/pointers2.c
+++ pointers/pointers2.c
@@ -2,11 +2,13 @@
 
 void main(void) {
   int target, source;
-  int *m;
+  int *m = 0;
 
   source = 710;
-  m = &source;
-  target = *m;
+  m = &source; // m agora tem o endereço de source.
+  target = *m; // agora target aponta pro que tem no endereço de m
 
-  printf("%d", target);
+  printf("%d\n", target);
+  printf("%d\n", &source);
+  printf("%d\n", m);
 }
